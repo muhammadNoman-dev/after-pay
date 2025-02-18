2684fce088fdf76a84dfe1ed4a989d3fdcb6c26f
noman <noman@technevity.net>
2025-02-18 18:27:48 +0500

    changes for vercel

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from './config/config.service';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Callback, Context, Handler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,11 +21,19 @@ async function bootstrap() {
     swaggerOptions: { persistAuthorization: true },
   });
 
-  const appConfig = app.get(ConfigService);
   app.enableCors();
-  await app.listen(appConfig.port);
-  console.log('Swagger Docs available at /docs')
-  console.log(`Application is running on: http://localhost:${appConfig.port}/docs`)
-  
+
+  await app.init(); // Initialize the app without calling `listen()`
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  const server = await bootstrap();
+  return server(event, context, callback);
+};
